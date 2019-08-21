50d883149a59e2113df982e21152cc3d3d5dcf4b
John Travers <johnk.travers@gmail.com>
2019-08-20 22:07:09 -0600

    Complete iteration 4

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -1,3 +1,6 @@
+require './lib/dish'
+require 'active_support/inflector'
+
 class Potluck
   attr_reader :date, :dishes
 
@@ -14,6 +17,64 @@ class Potluck
     @dishes.find_all do |dish|
       dish.category == category
     end
+  end
+
+  def menu
+    menu = {}
+
+    # For each category and dish
+    get_categories.each do |cat|
+      @dishes.each do |dish|
+
+        # If the dish's category matches the iteration
+        if dish.category == cat
+
+          # Pluralize the category name
+          plural = cat.to_s.pluralize.to_sym
+
+          # If the plural category already exists as a key in the hash
+          if menu[plural].class == Array
+            # Add the dish name to array
+            menu[plural].push(dish.name)
+
+          else
+            # Make a new array for that key and add the dish name
+            menu[plural] = []
+            menu[plural].push(dish.name)
+          end
+        end
+      end
+    end
+
+    # Alphabetize arrays of dish names within hash
+    menu.each do |category, array|
+      menu[category].sort!
+    end
+
+    menu
+  end
+
+  def ratio(category)
+    num_dishes = {}
+
+    # Create a hash with category as the key and number of dishes in that
+    # category as the value
+    get_categories.each do |cat|
+      num_dishes[cat] = get_all_from_category(cat).length
+    end
+
+    # Calculate ratio of dishes in that category to total dishes
+    num_dishes[category].to_f / num_dishes.values.sum * 100
+  end
+
+  def get_categories
+    # Create an array of categories
+    categories = @dishes.map do |dish|
+      dish.category
+    end
+
+    # Return only unique categories
+    categories.uniq
   end
 
 end
--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -11,6 +11,7 @@ class PotluckTest < Minitest::Test
     @roast_pork = Dish.new("Roast Pork", :entre)
     @cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
     @candy_salad = Dish.new("Candy Salad", :dessert)
+    @bean_dip = Dish.new("Bean Dip", :appetizer)
 
     @potluck = Potluck.new("7-13-18")
   end
@@ -53,6 +54,46 @@ class PotluckTest < Minitest::Test
     assert_equal [@couscous_salad, @summer_pizza], @potluck.get_all_from_category(:appetizer)
     assert_equal [@roast_pork, @cocktail_meatballs], @potluck.get_all_from_category(:entre)
     assert_equal [@candy_salad], @potluck.get_all_from_category(:dessert)
+  end
+
+  def test_menu_creates_a_hash_of_dishes_by_category
+    @potluck.add_dish(@couscous_salad)
+    @potluck.add_dish(@summer_pizza)
+    @potluck.add_dish(@roast_pork)
+    @potluck.add_dish(@cocktail_meatballs)
+    @potluck.add_dish(@candy_salad)
+    @potluck.add_dish(@bean_dip)
+
+    expected = {:appetizers => ["Bean Dip", "Couscous Salad", "Summer Pizza"],
+                :entres => ["Cocktail Meatballs", "Roast Pork"],
+                :desserts => ["Candy Salad"]
+               }
+
+    assert_equal expected, @potluck.menu
+  end
+
+  def test_ratio_of_dishes_by_category
+    @potluck.add_dish(@couscous_salad)
+    @potluck.add_dish(@summer_pizza)
+    @potluck.add_dish(@roast_pork)
+    @potluck.add_dish(@cocktail_meatballs)
+    @potluck.add_dish(@candy_salad)
+    @potluck.add_dish(@bean_dip)
+
+    assert_equal 50.0, @potluck.ratio(:appetizer)
+    assert_equal 33.33, @potluck.ratio(:entre).round(2)
+    assert_equal 16.67, @potluck.ratio(:dessert).round(2)
+  end
+
+  def test_get_categories_returns_array_of_categories
+    @potluck.add_dish(@couscous_salad)
+    @potluck.add_dish(@summer_pizza)
+    @potluck.add_dish(@roast_pork)
+    @potluck.add_dish(@cocktail_meatballs)
+    @potluck.add_dish(@candy_salad)
+    @potluck.add_dish(@bean_dip)
+
+    assert_equal [:appetizer, :entre, :dessert], @potluck.get_categories
   end
 
 end
